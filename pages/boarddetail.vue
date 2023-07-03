da907a7681699c417ac4ab35f3a88cd468c03600
황승현 <ghkdsigm3@gmail.com>
2023-07-03 11:20:43 +0900

    포커스 와치

--- pages/boarddetail.vue
+++ pages/boarddetail.vue
@@ -45,8 +45,8 @@
                     <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path></svg>
                     <span class="sr-only">Upload image</span>
                 </button>
-                <textarea id="chat" rows="1" class="block p-2.5 mx-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="댓글을 남겨주세요..."></textarea>
-                <button type="submit" class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
+                <textarea id="chat" rows="1" @keyup="focusText" class="block p-2.5 mx-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="댓글을 남겨주세요..."></textarea>
+                <button ref="commentbt" type="submit" :class="[emptyValue ? 'text-gray-400' : 'text-blue-600']" class="inline-flex justify-center p-2 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
                     <svg aria-hidden="true" class="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
                     <span class="sr-only">Send message</span>
                 </button>
@@ -149,14 +149,43 @@
 export default {
   data() {
     return {
-      cont: ""
+      cont: "",
+      cbt: "",
+      emptyValue : true
     }
   },
   mounted() {
     if (this.$route.params.id) {
       this.cont = this.$route.params.id
     }
+  },
+  methods:{
+    focusText(newValue, oldValue){
+      if(newValue.target.value !== ""){
+        this.emptyValue = false
+      } else {
+        this.emptyValue = true
+      }      
+    }
+  },
+  watch:{
+    focusText(newValue, oldValue) {
+      // methods에 정의된 함수를 활용하여 데이터의 변화를 처리      
+      this(newValue, oldValue);
+    }
   }
+  // computed:{
+  //   // eslint-disable-next-line vue/return-in-computed-property
+  //   focusText(e){   
+  //     console.log(e) 
+  //     // if(e.target.value !== ""){ 
+  //     //   this.emptyValue = true
+  //     // } else {
+  //     //   this.cbt = ""
+  //     //   this.emptyValue = false
+  //     // }
+  //   }
+  // }
 }
 </script>
 
