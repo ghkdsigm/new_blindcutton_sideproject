1dcbdbc968257316b2164ed99f341f4c0e83d759
황승현 <ghkdsigm3@gmail.com>
2023-07-04 11:51:19 +0900

    오늘의 이야기 게시글 업데이트

--- components/topic/TopicList.vue
+++ components/topic/TopicList.vue
@@ -3,13 +3,13 @@
     <h3 class="w-full border-b border-slate-300 flex justify-between text-xl font-bold mb-4 items-center py-2 tracking-tighter">오늘의 이야기 <a href="" class="text-xs font-normal">더보기</a></h3>
     <ul>
       <li class="pb-3 last:pb-0 flex justify-between tracking-tighter" v-for="(item, index) in this.listData" :key="index">
-        <span class="flex items-center w-11/12">        
+        <nuxt-link :to="{name:'boarddetail', params:{id: item}}" class="flex items-center w-11/12">        
           <span class="flex items-center border border-slate-300 px-2 text-xs mr-2 text-slate-500">{{ item.category }}</span> 
           <a href="" class="text-sm items-center max-w-[90%] truncate inline-block" :title="item.title">
-            <img src="https://www.pngkey.com/png/full/291-2912837_album-comments-album-icon-png.png" alt="" class="relative bottom-0.5 inline-block w-4" v-if="item.addedfile === 'Y'">
+            <img v-if="item.imgurl" :src="item.imgurl" alt="" class="relative bottom-0.5 inline-block w-4">
             {{ item.title }}
           </a>
-        </span>
+        </nuxt-link>
         <span class="w-1/12 flex justify-between text-left">
           <span class="w-1/2 text-xs text-gray-500 font-light"><i class="blind"></i>{{ item.views }}</span>
           <span class="w-1/2 text-xs text-gray-500 font-light"><i class="blind"></i>{{ item.likes }}</span>           
@@ -22,7 +22,7 @@
 <script>
 import data from '~/api/data.json'
 
-export default {
+export default {  
   data(){
     return {
       listData: ''
@@ -30,6 +30,7 @@ export default {
   },
   created(){
     this.listData = data.board
+    console.log(this.listData)
   }
 }
 </script>
