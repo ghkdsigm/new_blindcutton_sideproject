e615c9190cb5c1562e069883fc1e7afef9b8074d
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-07-05 23:35:59 +0900

    bg 및 dark모드 css 삭제

--- components/write/BoardWrite.vue
+++ components/write/BoardWrite.vue
@@ -1,101 +1,150 @@
 <template>
-    <div class="fixed z-10 inset-0 overflow-y-auto" >
-        <div class="flex justify-center min-h-screen sm:pt-6 sm:px-4 text-center sm:p-0 items-center">
-            <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-            <div class="absolute inset-0 bg-black dark:bg-gray-800 opacity-80"></div>
-            </div>
+  <div class="fixed z-10 inset-0 overflow-y-auto">
+    <div
+      class="flex justify-center min-h-screen sm:pt-6 sm:px-4 text-center sm:p-0 items-center"
+    >
+      <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div class="absolute inset-0 bg-black opacity-80"></div>
+      </div>
 
-            <!-- 상단 -->
-            <div @click.stop class="flex flex-col max-w-3xl h-[60vh] bg-white dark:bg-black text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle w-full justify-center" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-                <div class="border-b border-gray-200 dark:border-gray-700 p-2 text-right flex items-center justify-between px-7 py-2">
-                    <button @click="sendClose()" class="fas fa-times text-primary dark:text-white text-base">닫기</button>
-                    <span class="text-lg font-bold">글쓰기</span>
-                    <!-- 글쓰기버튼 -->
-                    <div class="lg:text-right mr-2">
-                        <button v-if="this.tBody !== '' && this.tTitle !== '' && this.selected !== '0'" @click="onAddcont" class="bg-primary text-blue-600 text-base hover:bg-dark font-medium py-1 rounded-full">등록</button>
-                        <button v-else @click="nonAddcont" class="bg-light text-gray-400 text-base font-medium  py-1 rounded-full cursor-auto">등록</button>
-                    </div>
-                </div>            
-                
-                <!-- 트윗섹션 -->
-                <div class="flex p-4 flex-col flex-1 h-full">
-                    <div class="mx-3 h-full flex flex-col">
-                        <!-- 카테고리 -->
-                        <div>
-                            <select ref="category" v-model="selected" class="w-full cursor-pointer text-lg font-medium focus:outline-none mb-4 pb-4 resize-none">
-                                <option v-for="option in options" :value="option.value" :key="option.value" class="px-0">
-                                    {{ option.text }}
-                                </option>
-                            </select>
-                        </div>
-                        <!-- 제목 -->
-                        <input type="text" ref="tTitlezone" v-model="tTitle" placeholder="제목을 입력하세요." class="placeholder-gray-300 border-b border-slate-300 w-full dark:bg-black dark:text-white text-lg font-medium focus:outline-none mb-4 pb-4 resize-none" />
-                        
-                        <!-- 내용 -->
-                        <textarea ref="tBodyzone" v-model="tBody" rows="5" placeholder="토픽에 맞지 않는 글로 판단되어 다른 유저로부터 일정 수 이상의 신고를 받는 경우 글이 자동으로 숨김처리 될 수 있습니다." class="flex flex-1 flex-col border border-white focus:border-gray-300 placeholder-gray-400 placeholder-opacity-75 w-full dark:bg-black dark:text-white text-normal font-light focus:outline-none mb-3 resize-none"></textarea>
-                        <!-- 스몰일때 트윗버튼 -->
-                        <!-- <div class="text-right hidden sm:block">
+      <!-- 상단 -->
+      <div
+        @click.stop
+        class="flex flex-col max-w-3xl h-[60vh] bg-white text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle w-full justify-center"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-headline"
+      >
+        <div
+          class="border-b border-gray-200 p-2 text-right flex items-center justify-between px-7 py-2"
+        >
+          <button @click="sendClose()" class="fas fa-times text-primary">
+            닫기
+          </button>
+          <span class="text-lg font-bold">글쓰기</span>
+          <!-- 글쓰기버튼 -->
+          <div class="lg:text-right mr-2">
+            <button
+              v-if="
+                this.tBody !== '' && this.tTitle !== '' && this.selected !== '0'
+              "
+              @click="onAddcont"
+              class="bg-primary text-blue-600 text-base font-medium py-1 rounded-full"
+            >
+              등록
+            </button>
+            <button
+              v-else
+              @click="nonAddcont"
+              class="bg-light text-gray-400 text-base font-medium py-1 rounded-full cursor-auto"
+            >
+              등록
+            </button>
+          </div>
+        </div>
+
+        <!-- 트윗섹션 -->
+        <div class="flex p-4 flex-col flex-1 h-full">
+          <div class="mx-3 h-full flex flex-col">
+            <!-- 카테고리 -->
+            <div>
+              <select
+                ref="category"
+                v-model="selected"
+                class="w-full cursor-pointer text-lg font-medium focus:outline-none mb-4 pb-4 resize-none"
+              >
+                <option
+                  v-for="option in options"
+                  :value="option.value"
+                  :key="option.value"
+                  class="px-0"
+                >
+                  {{ option.text }}
+                </option>
+              </select>
+            </div>
+            <!-- 제목 -->
+            <input
+              type="text"
+              ref="tTitlezone"
+              v-model="tTitle"
+              placeholder="제목을 입력하세요."
+              class="placeholder-gray-300 border-b border-slate-300 w-full text-lg font-medium focus:outline-none mb-4 pb-4 resize-none"
+            />
+
+            <!-- 내용 -->
+            <textarea
+              ref="tBodyzone"
+              v-model="tBody"
+              rows="5"
+              placeholder="토픽에 맞지 않는 글로 판단되어 다른 유저로부터 일정 수 이상의 신고를 받는 경우 글이 자동으로 숨김처리 될 수 있습니다."
+              class="flex flex-1 flex-col border border-white focus:border-gray-300 placeholder-gray-400 placeholder-opacity-75 w-full text-normal font-light focus:outline-none mb-3 resize-none"
+            ></textarea>
+            <!-- 스몰일때 트윗버튼 -->
+            <!-- <div class="text-right hidden sm:block">
                             <button v-if="!textBody.length" class="bg-light text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
                             <button v-else @click="onAddTweet" class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
                         </div> -->
-                    </div>                    
-                </div>
-                <!-- 하단 -->
-                <div clas="w-full bg-black h-2">
-                    <img class="w-12 h-12 rounded-full hover:opacity-80 cursor-pointer" /> 
-                </div>
-            </div>            
+          </div>
         </div>
+        <!-- 하단 -->
+        <div clas="w-full bg-black h-2">
+          <img class="w-12 h-12 rounded-full hover:opacity-80 cursor-pointer" />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
-  
+
 <script>
 export default {
-    data() {
-        return {
-            tTitle: "",
-            tBody: "",
-            selected: '0',
-            options: [
-                { text: '카테고리를 선택하세요', value: '0' },
-                { text: '부동산', value: '1' },
-                { text: '육아', value: '2' },
-                { text: '자동차', value: '3' },
-                { text: '연예', value: '4' },
-                { text: '재테크', value: '5' }
-            ]
-        }
+  data() {
+    return {
+      tTitle: "",
+      tBody: "",
+      selected: "0",
+      options: [
+        { text: "카테고리를 선택하세요", value: "0" },
+        { text: "부동산", value: "1" },
+        { text: "육아", value: "2" },
+        { text: "자동차", value: "3" },
+        { text: "연예", value: "4" },
+        { text: "재테크", value: "5" },
+      ],
+    };
+  },
+  methods: {
+    onAddcont() {
+      alert("ok");
+      this.$emit("close-modal");
     },
-    methods:{
-        onAddcont(){
-            alert('ok')
-            this.$emit('close-modal');
-        },
-        nonAddcont(){
-            if(this.tTitle === ""){
-                alert('제목을 입력하세요!')
-                this.$refs.tTitlezone.focus()
-            } 
-            else if(this.tBody === ""){
-                alert('내용을 입력하세요!')
-                this.$refs.tBodyzone.focus()
-            }
-            else if(this.selected === "0"){
-                alert('카테고리를 선택하세요!')
-                this.$refs.category.focus()
-            }
-        },
-        sendClose(){
-            if(this.tTitle === '' && this.tBody === ''){
-                this.$emit('close-modal');
-            } else {
-                if(confirm('작성을 취소하시면 작성히신 내용이 사라집니다. \n그래도 작성을 취소하시겠습니까?')){
-                    this.$emit('close-modal');
-                }
-            }
+    nonAddcont() {
+      if (this.tTitle === "") {
+        alert("제목을 입력하세요!");
+        this.$refs.tTitlezone.focus();
+      } else if (this.tBody === "") {
+        alert("내용을 입력하세요!");
+        this.$refs.tBodyzone.focus();
+      } else if (this.selected === "0") {
+        alert("카테고리를 선택하세요!");
+        this.$refs.category.focus();
+      }
+    },
+    sendClose() {
+      if (this.tTitle === "" && this.tBody === "") {
+        this.$emit("close-modal");
+      } else {
+        if (
+          confirm(
+            "작성을 취소하시면 작성히신 내용이 사라집니다. \n그래도 작성을 취소하시겠습니까?"
+          )
+        ) {
+          this.$emit("close-modal");
         }
-    }
-}
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
--- pages/boarddetail.vue
+++ pages/boarddetail.vue
@@ -4,21 +4,37 @@
       <div class="w-full border-b border-gray-200 pb-8">
         <h1>
           <span href="" class="font-bold tracking-tighter flex items-center">
-            <nuxt-link :to="{name:'category', params:{id: 'board'}}" class="h-full text-sm font-semibold">토픽</nuxt-link> <span class="text-xs px-3 font-mono h-full">></span> <span class="h-full text-sm font-semibold">{{ cont.category }}</span>
+            <nuxt-link
+              :to="{ name: 'category', params: { id: 'board' } }"
+              class="h-full text-sm font-semibold"
+              >토픽</nuxt-link
+            >
+            <span class="text-xs px-3 font-mono h-full">></span>
+            <span class="h-full text-sm font-semibold">{{
+              cont.category
+            }}</span>
           </span>
         </h1>
         <h2 class="text-2xl my-3 font-bold tracking-tighter">
           {{ cont.title }}
         </h2>
         <div>
-          <a href="" class="text-sm font-medium text-sky-500">{{ cont.writer }}</a>
+          <a href="" class="text-sm font-medium text-sky-500">{{
+            cont.writer
+          }}</a>
           <span></span>
         </div>
         <div>
           <span>
-            <span class="text-xs text-gray-500 font-medium">{{ cont.date }}</span>
-            <span class="text-xs text-gray-500 font-medium">{{ cont.views }}</span>
-            <span class="text-xs text-gray-500 font-medium">{{ cont.likes }}</span>
+            <span class="text-xs text-gray-500 font-medium">{{
+              cont.date
+            }}</span>
+            <span class="text-xs text-gray-500 font-medium">{{
+              cont.views
+            }}</span>
+            <span class="text-xs text-gray-500 font-medium">{{
+              cont.likes
+            }}</span>
           </span>
           <span>
             <span></span>
@@ -31,64 +47,109 @@
           {{ cont.content }}
         </p>
         <div v-if="cont.imgurl" class="pt-8">
-          <img :src="cont.imgurl" alt="">
+          <img :src="cont.imgurl" alt="" />
         </div>
-        <div class="flex pt-8 ">
+        <div class="flex pt-8">
           <span class="tracking-tighter">{{ cont.recommentcount }}</span>
         </div>
         <ul class="flex pt-8">
           <li v-for="(item, index) in cont.hashtag" :key="index">
             <span class="pr-2 font-mono text-xs text-gray-600 tracking-tight">
-              {{ '#' + item }}
+              {{ "#" + item }}
             </span>
           </li>
         </ul>
       </div>
       <!-- 댓글존 -->
       <div class="w-full border-t border-b border-gray-200">
-        <p class="pt-4 pl-2 pr-2 text-base font-bold tracking-tight">댓글 <span>14</span></p>
-        <div class="write_area  pb-4">
+        <p class="pt-4 pl-2 pr-2 text-base font-bold tracking-tight">
+          댓글 <span>14</span>
+        </p>
+        <div class="write_area pb-4">
           <form>
             <label for="chat" class="sr-only">Your message</label>
             <div class="flex items-center py-2 rounded-lg">
-                <button type="button" class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                    <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path></svg>
-                    <span class="sr-only">Upload image</span>
-                </button>
-                <textarea ref="comment" id="chat" rows="1" @keyup="focusText" class="block p-2.5 mx-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="댓글을 남겨주세요..."></textarea>
-                <span @click="commentbt" type="submit" :class="[emptyValue ? 'text-gray-400 hover:bg-gray-100' : 'text-blue-600 hover:bg-blue-100']" class="inline-flex justify-center p-2 rounded-full cursor-pointer dark:text-blue-500 dark:hover:bg-gray-600">
-                    <svg aria-hidden="true" class="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
-                    <span class="sr-only">Send message</span>
-                </span>
+              <button
+                type="button"
+                class="inline-flex justify-center p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span class="sr-only">Upload image</span>
+              </button>
+              <textarea
+                ref="comment"
+                id="chat"
+                rows="1"
+                @keyup="focusText"
+                class="block p-2.5 mx-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                placeholder="댓글을 남겨주세요..."
+              ></textarea>
+              <span
+                @click="commentbt"
+                type="submit"
+                :class="[
+                  emptyValue
+                    ? 'text-gray-400 hover:bg-gray-100'
+                    : 'text-blue-600 hover:bg-blue-100',
+                ]"
+                class="inline-flex justify-center p-2 rounded-full cursor-pointer"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="w-6 h-6 rotate-90"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
+                  ></path>
+                </svg>
+                <span class="sr-only">Send message</span>
+              </span>
             </div>
-          </form>          
+          </form>
         </div>
       </div>
       <!-- //댓글존 -->
       <!-- 대댓글존 -->
       <div class="w-full p-4 border-b border-gray-200">
         <p class="flex">
-          <a href="" class="text-sm font-medium text-sky-500">{{ cont.writer }}</a>
+          <a href="" class="text-sm font-medium text-sky-500">{{
+            cont.writer
+          }}</a>
         </p>
-        <p class=" text-base tracking-tighter pt-2 pb-4">
+        <p class="text-base tracking-tighter pt-2 pb-4">
           {{ cont.content }}
-        </p>        
+        </p>
         <div class="flex justify-between">
           <span class="text-xs">
             <span></span>
             <a href="">좋아요</a>
             <a href="">대댓글</a>
           </span>
-          <span class="text-xs">
-            대댓글쓰기
-          </span>
+          <span class="text-xs"> 대댓글쓰기 </span>
         </div>
       </div>
       <div class="w-full p-4 border-b border-gray-200">
         <p class="flex">
-          <a href="" class="text-sm font-medium text-sky-500">{{ cont.writer }}</a>
+          <a href="" class="text-sm font-medium text-sky-500">{{
+            cont.writer
+          }}</a>
         </p>
-        <p class=" text-base tracking-tighter pt-2 pb-4">
+        <p class="text-base tracking-tighter pt-2 pb-4">
           {{ cont.content }}
         </p>
         <div class="flex justify-between">
@@ -97,23 +158,26 @@
             <a href="">좋아요</a>
             <a href="">대댓글</a>
           </span>
-          <span class="text-xs">
-            대댓글쓰기
-          </span>
+          <span class="text-xs"> 대댓글쓰기 </span>
         </div>
       </div>
       <!-- //대댓글존 -->
       <!-- View 광고존 -->
       <div class="w-full mt-10 mb-6">
         <a href="">
-          <img src="http://d2u3dcdbebyaiu.cloudfront.net/img/web_banner/web_banner_kr_1683619733.jpg" alt="">
+          <img
+            src="http://d2u3dcdbebyaiu.cloudfront.net/img/web_banner/web_banner_kr_1683619733.jpg"
+            alt=""
+          />
         </a>
       </div>
       <!-- //View 광고존 -->
-    </div>    
+    </div>
     <div class="w-2/7">
       <div class="mb-8 border border-slate-200 p-6">
-        <strong class="flex w-full text-sm font-bold mb-4">'{{ cont.category }}'관련 추천글</strong>
+        <strong class="flex w-full text-sm font-bold mb-4"
+          >'{{ cont.category }}'관련 추천글</strong
+        >
         <!-- 관련 추천글 -->
         <ul class="">
           <li
@@ -122,14 +186,20 @@
             :key="index"
           >
             <span class="w-full text-sm flex justify-between">
-              <span class="flex w-full"><em class="not-italic font-bold text-gray-400 mr-2 w-4 inline-block text-center text-sm"
+              <span class="flex w-full"
+                ><em
+                  class="not-italic font-bold text-gray-400 mr-2 w-4 inline-block text-center text-sm"
                   >{{ index + 1 }}</em
                 >
-                <span class="text-sm truncate tracking-tighter hover:cursor-pointer" @click="viewRelative(item)">{{ item.title }}</span>
-              </span>                
+                <span
+                  class="text-sm truncate tracking-tighter hover:cursor-pointer"
+                  @click="viewRelative(item)"
+                  >{{ item.title }}</span
+                >
+              </span>
             </span>
           </li>
-        </ul>   
+        </ul>
         <!-- //관련 추천글 -->
       </div>
       <a href="" class="my-8 block">
@@ -158,60 +228,66 @@ export default {
     return {
       cont: "",
       cbt: "",
-      emptyValue : true,
-      recommend: '',
-      newarr: []
-    }
+      emptyValue: true,
+      recommend: "",
+      newarr: [],
+    };
   },
   mounted() {
     if (this.$route.params.id) {
-      this.cont = this.$route.params.id
+      this.cont = this.$route.params.id;
     }
 
-    if(data[this.cont.type]){
-      for(let i = 0; i < data[this.cont.type].length; i++){
-        if(this.cont.category === data[this.cont.type][i].category && this.cont.id !== data[this.cont.type][i].id){
-        this.newarr.push(data[this.cont.type][i])
+    if (data[this.cont.type]) {
+      for (let i = 0; i < data[this.cont.type].length; i++) {
+        if (
+          this.cont.category === data[this.cont.type][i].category &&
+          this.cont.id !== data[this.cont.type][i].id
+        ) {
+          this.newarr.push(data[this.cont.type][i]);
         }
       }
     }
   },
-  methods:{
-    focusText(newValue, oldValue){
-      if(newValue.target.value !== ""){
-        this.emptyValue = false
-        this.$refs.comment.setAttribute('rows', 5)
+  methods: {
+    focusText(newValue, oldValue) {
+      if (newValue.target.value !== "") {
+        this.emptyValue = false;
+        this.$refs.comment.setAttribute("rows", 5);
       } else {
-        this.emptyValue = true
-        this.$refs.comment.setAttribute('rows', 1)
-      }      
+        this.emptyValue = true;
+        this.$refs.comment.setAttribute("rows", 1);
+      }
     },
-    commentbt(){
-      if(this.emptyValue === true){
-        alert('내용을 입력해주세요')
-        this.$refs.comment.focus()
-      } 
+    commentbt() {
+      if (this.emptyValue === true) {
+        alert("내용을 입력해주세요");
+        this.$refs.comment.focus();
+      }
     },
-    viewRelative(e){
-      this.cont = e      
-      
-      if(data[this.cont.type]){
-        for(let i = 0; i < data[this.cont.type].length; i++){
-          if(this.cont.category === data[this.cont.type][i].category && this.cont.id !== data[this.cont.type][i].id){
-            this.newarr = []
-            this.newarr.push(data[this.cont.type][i])
+    viewRelative(e) {
+      this.cont = e;
+
+      if (data[this.cont.type]) {
+        for (let i = 0; i < data[this.cont.type].length; i++) {
+          if (
+            this.cont.category === data[this.cont.type][i].category &&
+            this.cont.id !== data[this.cont.type][i].id
+          ) {
+            this.newarr = [];
+            this.newarr.push(data[this.cont.type][i]);
           }
         }
       }
-    }
+    },
   },
-  watch:{
+  watch: {
     focusText(newValue, oldValue) {
-      // methods에 정의된 함수를 활용하여 데이터의 변화를 처리      
+      // methods에 정의된 함수를 활용하여 데이터의 변화를 처리
       this(newValue, oldValue);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style></style>
